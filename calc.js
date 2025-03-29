450420387157fdc3f9f365fd873530609a50542c
GARVIT BAJAJ <bajajarjun020@gmail.com>
2025-03-29 23:45:16 +0530

    Update calc.js

--- calc.js
+++ calc.js
@@ -1,17 +1,30 @@
 const display = document.querySelector("#display");
 
-function appendToDisplay(keyinputs){
+function appendToDisplay(keyinputs) {
     display.value += keyinputs;
 }
 
-function clearDisplay(){
+function clearDisplay() {
     display.value = "";
 }
 
-function calculate(){
-    try{
+function calculate() {
+    try {
         display.value = eval(display.value);
-    }catch(error){
+    } catch (error) {
         display.value = "Error";
-    } 
+    }
 }
+
+// Key event listener
+document.addEventListener("keydown", function (event) {
+    if (/[\d+\-*/.]/.test(event.key)) {
+        appendToDisplay(event.key);
+    } else if (event.key === "Enter") {
+        calculate();
+    } else if (event.key === "Backspace") {
+        display.value = display.value.slice(0, -1);
+    } else if (event.key === "Escape" || event.key === "Delete") {
+        clearDisplay();
+    }
+});
